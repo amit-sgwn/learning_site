59f227dd15a109cb5d5c236e91fb37acc2a65d08
amit-sgwn <amitsangwan898@gmail.com>
2016-12-29 19:39:14 +0530

    Added a test for model testing

--- courses/tests.py
+++ courses/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
-
+from  django.utils import timezone
 # Create your tests here.
+from .models import Course
+
+class CourseModelTest(TestCase):
+    def test_course_creation(self):
+        course = Course.objects.create(title="Python Regular Expression",description="Lerning Regular Expression")
+        now =timezone.now()
+        self.assertLess(course.created_at,now)
